42c5e6f966bf23037705c2b8567895641bb1c98f
argostellar <arskud@gmail.com>
2019-07-22 00:17:42 +0300

    Исправления линтера

--- js/picture.js
+++ js/picture.js
@@ -6,7 +6,7 @@
   var image = picture.querySelector('.big-picture__img');
   var likesCount = picture.querySelector('.likes-count');
   var commentsCount = picture.querySelector('.comments-count');
-  var comments = picture.querySelector('.social__comments');
+  // var comments = picture.querySelector('.social__comments');
   var description = picture.querySelector('.social__caption');
   var commentsMeter = picture.querySelector('.social__comment-count');
   var commentsLoader = picture.querySelector('.comments-loader');
@@ -15,7 +15,7 @@
   // commentsMeter.classList.add('visually-hidden');
   // commentsLoader.classList.add('visually-hidden');
 
-  var renderComment = function (commentData) {
+  var renderComment = function () {
     var AVATAR_AMOUNT = 6;
     var AVATAR_WIDTH = 35;
     var AVATAR_HEIGHT = 35;
