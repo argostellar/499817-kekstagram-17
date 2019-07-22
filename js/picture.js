a6f75d7e68bb6a7a681803a0a470cecb1899c72e
argostellar <arskud@gmail.com>
2019-07-22 14:43:54 +0300

    Доработка отображения

--- js/picture.js
+++ js/picture.js
@@ -6,39 +6,54 @@
   var image = picture.querySelector('.big-picture__img');
   var likesCount = picture.querySelector('.likes-count');
   var commentsCount = picture.querySelector('.comments-count');
-  // var comments = picture.querySelector('.social__comments');
-  var description = picture.querySelector('.social__caption');
-  var commentsMeter = picture.querySelector('.social__comment-count');
   var commentsLoader = picture.querySelector('.comments-loader');
+
+  var social = picture.querySelector('.social');
+  var commentsField = social.querySelector('.social__comments');
+  var description = social.querySelector('.social__caption');
+  var commentsMeter = social.querySelector('.social__comment-count');
+
 
   // window.utility.open(picture);
   // commentsMeter.classList.add('visually-hidden');
   // commentsLoader.classList.add('visually-hidden');
 
-  var renderComment = function () {
-    var AVATAR_AMOUNT = 6;
+  var renderComment = function (commentData) {
+    // var AVATAR_AMOUNT = 6;
     var AVATAR_WIDTH = 35;
     var AVATAR_HEIGHT = 35;
-    var random = window.utility.getRandomNumber(AVATAR_AMOUNT, true);
+    // var random = window.utility.getRandomNumber(AVATAR_AMOUNT, true);
     var comment = document.createElement('li');
     comment.classList.add('social__comment');
     var avatar = document.createElement('img');
     comment.insertAdjacentElement('afterbegin', avatar);
-    avatar.src = 'img/avatar-' + random + '.svg';
+    // avatar.src = 'img/avatar-' + random + '.svg';
+    avatar.src = commentData.avatar;
     avatar.alt = 'Аватар комментатора фотографии';
     avatar.width = AVATAR_WIDTH;
     avatar.height = AVATAR_HEIGHT;
     var text = document.createElement('p');
-    comment.insertAdjacentElement('afterbegin', avatar);
+    comment.insertAdjacentElement('beforeend', text);
     text.classList.add('social__text');
-    // text.textContent = commentData.message;
-    text.textContent = '{{текст комментария}}';
+    text.textContent = commentData.message;
+    // text.textContent = '{{текст комментария}}';
+    commentsField.appendChild(comment);
+  };
+
+  var clearCommentField = function () {
+    var commentsCollection = social.querySelectorAll('.social__comment');
+    var commentsArray = Array.from(commentsCollection);
+    commentsArray.forEach(function (comment) {
+      comment.remove();
+    });
+    // commentsField.removeChild();
   };
 
   var openPicture = function (photo) {
     window.utility.open(picture);
     commentsMeter.classList.add('visually-hidden');
     commentsLoader.classList.add('visually-hidden');
+    clearCommentField();
     image.src = photo.url;
     likesCount.textContent = photo.likes;
     commentsCount.textContent = photo.comments.length;
