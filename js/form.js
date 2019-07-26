e8da5974a24256717e2066520a65be42c0c68b64
argostellar <arskud@gmail.com>
2019-07-26 13:40:40 +0300

    Реализация загрузки данных на сервер

--- js/form.js
+++ js/form.js
@@ -336,4 +336,63 @@
 
   uploadCancel.addEventListener('keydown', onClosePressEsc);
 
+  var successTemplate = document.querySelector('#success');
+  var errorTemplate = document.querySelector('#error');
+  var main = document.querySelector('main');
+
+  var createMessage = function (template) {
+    var message = template.content.cloneNode(true);
+    main.appendChild(message);
+    if (template === successTemplate) {
+      var successButton = message.querySelector('.success__button');
+      var onSuccessButtonClick = function () {
+        window.utility.close(message);
+        successButton.removeEventListener('click', onSuccessButtonClick);
+      };
+      successButton.addEventListener('click', onSuccessButtonClick);
+    } else if (template === errorTemplate) {
+      var errorButton = message.querySelector('.error__button');
+      var onErrorButtonClick = function () {
+        window.utility.close(message);
+        errorButton.removeEventListener('click', onErrorButtonClick);
+      };
+      errorButton.addEventListener('click', onErrorButtonClick);
+    }
+
+    var onEscPressClose = function (evt) {
+      if (evt.keycode === window.global.ESC) {
+        window.utility.close(message);
+        document.removeEventListener('keydown', onEscPressClose);
+      }
+    };
+
+    var onOutsideAreaClickClose = function (evt) {
+      if (evt.currentTarget !== message) {
+        window.utility.close(message);
+        document.removeEventListener('click', onOutsideAreaClickClose);
+      }
+    };
+
+    document.addEventListener('keydown', onEscPressClose);
+    document.addEventListener('click', onOutsideAreaClickClose);
+  };
+
+  var onLoadSuccess = function () {
+    createMessage(successTemplate);
+    // console.log(message);
+  };
+
+  var onLoadError = function () {
+    createMessage(errorTemplate);
+    // console.log(message);
+  };
+
+  var onFormSubmissionSend = function (evt) {
+    evt.preventDefault();
+    window.utility.close(uploadForm);
+    window.server.save(new FormData(uploadForm), onLoadSuccess, onLoadError);
+  };
+
+  uploadForm.addEventListener('submit', onFormSubmissionSend);
+
 })();
